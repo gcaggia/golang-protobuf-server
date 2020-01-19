9681f23e94e42025283adca15cc7503226331d80
Guillaume Caggia <gcaggia@deputy.com>
2020-01-19 15:33:07 +1100

    add userJSON struct and parse post user endpoint

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/gcaggia/golang-protobuf-server/protodef"
 	"github.com/golang/protobuf/proto"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
@@ -12,6 +13,13 @@ import (
 
 func init() {
 	log.Println("App is running on port 18000")
+}
+
+type UserJson struct {
+	Id     int64  `json:"id"`
+	Name   string `json:"name"`
+	Email  string `json:"email"`
+	Active bool   `json:"active"`
 }
 
 type UserData struct {
@@ -46,7 +54,7 @@ func main() {
 
 		data, err := proto.Marshal(user)
 		if err != nil {
-			log.Fatal("marshal call error: ", err)
+			log.Fatal	("marshal call error: ", err)
 		}
 		// log raw protobuf object
 		log.Println("protobuf: " + byteToString(data))
@@ -58,6 +66,27 @@ func main() {
 		writer.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(writer).Encode(userData)
 	})
+
+	http.HandleFunc("/user", func(writer http.ResponseWriter, request *http.Request) {
+		if request.Method != "POST" {
+			http.NotFound(writer, request)
+			return
+		}
+
+		var userJson UserJson
+
+		reqBody, err := ioutil.ReadAll(request.Body)
+		err = json.Unmarshal(reqBody, &userJson)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Println("/user POST HTTP request")
+		json.NewEncoder(writer).Encode(userJson)
+		//d := json.NewDecoder(request.Body)
+	})
+
 
 	http.ListenAndServe("127.0.0.1:18000", nil)
 }
