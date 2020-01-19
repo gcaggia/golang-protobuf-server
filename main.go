e030a01eef0ffe4b3fc754767db26d296b718004
Guillaume Caggia <guillaume.caggia@gmail.com>
2020-01-20 08:45:23 +1100

    T-3 setup mux as router and home route

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"encoding/json"
+	"fmt"
 	"github.com/gcaggia/golang-protobuf-server/protodef"
-	"github.com/golang/protobuf/proto"
-	"io/ioutil"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"strconv"
@@ -35,58 +34,18 @@ func byteToString(b []byte) string {
 	return "[" + strings.TrimSpace(result) + "]"
 }
 
+func home(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Welcome to the protobuf server API!")
+	fmt.Println("Endpoint Hit: /")
+}
+
+func handleRequests() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", home)
+	log.Fatal(http.ListenAndServe(":18000", router))
+}
+
 func main() {
-
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		log.Println("Incoming request on '/'")
-		writer.Write([]byte("Received!"))
-	})
-
-	http.HandleFunc("/user/healthcheck", func(writer http.ResponseWriter, request *http.Request) {
-		log.Println("Incoming request on '/user/healthcheck'")
-
-		user := &protodef.User{
-			Id: 1,
-			Name: "Eric Freime",
-			Email: "efreime@gmail.com",
-			Active: true,
-		}
-
-		data, err := proto.Marshal(user)
-		if err != nil {
-			log.Fatal	("marshal call error: ", err)
-		}
-		// log raw protobuf object
-		log.Println("protobuf: " + byteToString(data))
-		userData := UserData{
-			User: user,
-			ProtoBuf: byteToString(data),
-		}
-		// writer.Write([]byte("Protobuf generated for id " + strconv.FormatInt(int64(user.Id), 10)))
-		writer.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(writer).Encode(userData)
-	})
-
-	http.HandleFunc("/user", func(writer http.ResponseWriter, request *http.Request) {
-		if request.Method != "POST" {
-			http.NotFound(writer, request)
-			return
-		}
-
-		var userJson UserJson
-
-		reqBody, err := ioutil.ReadAll(request.Body)
-		err = json.Unmarshal(reqBody, &userJson)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		log.Println("/user POST HTTP request")
-		json.NewEncoder(writer).Encode(userJson)
-		//d := json.NewDecoder(request.Body)
-	})
-
-
-	http.ListenAndServe("127.0.0.1:18000", nil)
+	fmt.Println("Rest API v1.0 - protobuf server")
+	handleRequests()
 }
